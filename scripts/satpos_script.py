91b868fa15ace1e2f3da5762d023df7e0e8f284f
Dave DeBoer <ddeboer@berkeley.edu>
2021-12-27 13:51:33 -0800

    change satpos_script to accommodate defaults

--- scripts/satpos_script.py
+++ scripts/satpos_script.py
@@ -4,8 +4,8 @@ import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument('tlefile', help="Name of tle file for which to generate satpos script.",
-                default='active')
-ap.add_argument('cfgfile', help="Name of config file for default tle path.",
+                default='active', nargs='?')
+ap.add_argument('--cfgfile', help="Name of config file for default tle path.",
                 default='satpos_cfg.yaml')
 ap.add_argument('-n', '--no_config_file', help="Flag to not include path from config.",
                 action='store_true')
